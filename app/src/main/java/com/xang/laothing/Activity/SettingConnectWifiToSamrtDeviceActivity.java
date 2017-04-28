109a49d93abb49f714a945b3f48ad0e0620511c7
xang <xang.ultimate@gmail.com>
2017-04-28 18:36:01 +0700

    handle connect wifi

---
+++ app/src/main/java/com/xang/laothing/Activity/SettingConnectWifiToSamrtDeviceActivity.java
@@ -0,0 +1,17 @@
+package com.xang.laothing.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xang.laothing.R;
+
+public class SettingConnectWifiToSamrtDeviceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_connect_wifi_to_samrt_device);
+    }
+
+
+}
